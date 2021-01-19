ba652a7c6274121020fa2b539b47ff6df84ab7f9
martacamacho <martacamachovargas@gmail.com>
2021-01-19 13:26:55 +0100

    navbar and menu done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import {Switch, Route, Redirect} from 'react-router-dom';
-import Login from './Components/auth/Login';
-import Logout from './Components/auth/Logout';
-import Register from './Components/auth/Register';
-import ForgotPassword from './Components/auth/ForgotPassword';
-import ResetPassword from './Components/auth/ResetPassword';
-import CheckInbox from './Components/auth/CheckInbox'; 
+import Login from './pages/auth/Login';
+import Logout from './pages/auth/Logout';
+import Register from './pages/auth/Register';
+import ForgotPassword from './pages/auth/ForgotPassword';
+import ResetPassword from './pages/auth/ResetPassword';
+import CheckInbox from './pages/auth/CheckInbox'; 
 import MenuAndNavbar from './Components/MenuAndNavbar';
 
 function App() {
   return (
     <div className="App">
+    {/* <MenuAndNavbar /> poner withauth cuando esté */}
       <Switch>
       <Redirect from exact path="/" to="/login"/>
       <Route exact path="/login" component={Login}/>
---
+++ src/pages/LandingNoCompanies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LandingNoCompanies() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LandingNoCompanies
