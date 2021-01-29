6a3faa0608d7452aa1651bf07012a1c018218ad5
martacamacho <martacamachovargas@gmail.com>
2021-01-29 12:18:09 +0100

    add and create company buttons added to landing companies

--- src/pages/LandingCompanies.jsx
+++ src/pages/LandingCompanies.jsx
@@ -3,80 +3,97 @@ import { withAuth } from "../lib/AuthProvider";
 import SideNavbar from "../Components/navbars/SideNavbar";
 import TopNavbar from "../Components/navbars/TopNavbar";
 import LandingNoCompanies from "./LandingNoCompanies";
-import companyservice from "../lib/company-service"
+import companyservice from "../lib/company-service";
+import { Link } from "react-router-dom";
 
 class LandingCompanies extends Component {
-  constructor(props){
-    super(props); 
-    this.state = { 
+  constructor(props) {
+    super(props);
+    this.state = {
       user: "",
       companies: [],
       companyName: "",
       employees: "",
-    }
-    
+    };
   }
 
   componentDidMount = async () => {
-    const companiesArr = await this.props.user.companies
-    console.log()
-      this.setState({
-        user: this.props.user,
-        companies: companiesArr,
-      })
-  }
+    const companiesArr = await this.props.user.companies;
+    console.log();
+    this.setState({
+      user: this.props.user,
+      companies: companiesArr,
+    });
+  };
 
   companyDetails = async (id) => {
-    const details = await companyservice.getCompany(id)
-    return details
-  }
+    const details = await companyservice.getCompany(id);
+    return details;
+  };
 
-
-/* TODO: hacer mapeo a las companies y traer datos */
+  /* TODO: hacer mapeo a las companies y traer datos (ahora solo trae el id que hay en el array de companies del usuario)*/
   render() {
     const { companies } = this.state;
-    console.log(this.state.companyName)
+    console.log(this.state.companyName);
     return (
       <>
-      { companies.length === 0 ? <LandingNoCompanies /> :
-      <div className="container">
-        <SideNavbar />
-        <div className="content">
-          <TopNavbar />
-          <div className="card mb-3">
-            <div className="card-header bg-light">
-              <h5 className="mb-0">Companies</h5>
-            </div>
-            { companies ? companies.map((company, index) => {
-                    return (
-            <div className="card-body fs--1 pb-0" key={index}>
-              <div className="row">
-                <div className="col-sm-6 col-md-4 mb-3">
-                  <div className="d-flex position-relative align-items-center mb-2">
-                    <img
-                      className="d-flex align-self-center mr-2"
-                      src=""
-                      alt="logo"
-                      width="50"
-                    />
-                    <div className="flex-1">
-                      <h6 className="fs-0 mb-0">
-                        <a className="stretched-link" href="#!">
-                          {company.companyName}
-                        </a>
-                      </h6>
-                      <p className="mb-1">3243 Personas</p>
-                    </div>
+        {companies.length === 0 ? (
+          <LandingNoCompanies />
+        ) : (
+          <div className="container">
+            <SideNavbar />
+            <div className="content">
+              <TopNavbar />
+              <div className="card mb-3">
+                <div className="card-header bg-light d-flex flex-column flex-md-row justify-content-between">
+                  <h5 className="mb-0 text-center">Companies</h5>
+                  <div className="d-flex flex-column flex-md-row mt-2 mt-md-0">
+                    <Link className="btn btn-falcon-primary mx-6 mx-md-0 mt-2 mt-md-0" to="/add-company">
+                      Add Company
+                    </Link>
+                    <Link
+                      className="btn btn-falcon-primary mx-6 mx-md-0 mt-2 mt-md-0 ml-md-2"
+                      to="/create-company-details"
+                    >
+                      Create Company
+                    </Link>
                   </div>
                 </div>
+                {companies ? (
+                  companies.map((company, index) => {
+                    return (
+                      <div className="card-body fs--1 pb-0" key={index}>
+                        <div className="row">
+                          <div className="col-sm-6 col-md-4 mb-3">
+                            <div className="d-flex position-relative align-items-center mb-2">
+                              <img
+                                className="d-flex align-self-center mr-2"
+                                src=""
+                                alt="logo"
+                                width="50"
+                              />
+                              <div className="flex-1">
+                                <h6 className="fs-0 mb-0">
+                                  <a className="stretched-link" href="#!">
+                                    {company}
+                                  </a>
+                                </h6>
+                                <p className="mb-1">3243 Personas</p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })
+                ) : (
+                  <p>Loading...</p>
+                )}
               </div>
             </div>
-            )}) : <p>Loading...</p>}
           </div>
-        </div>
-      </div>
-       }
-       </>
+        )}
+      </>
     );
   }
 }
